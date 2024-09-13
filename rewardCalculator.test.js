06c9ecd25b8d2d668e003a712c8ec2426d1c1302
PrabhuS <prabhuaspeare@gmail.com>
2024-09-13 19:08:54 +0530

    remove the test file

--- rewardCalculator.test.js
+++
@@ -1,7 +0,0 @@
-import { calculateRewards } from './rewardCalculator';
- 
-test('calculates rewards correctly', () => {
-  expect(calculateRewards(120)).toBe(90);
-  expect(calculateRewards(75)).toBe(25);
-  expect(calculateRewards(50)).toBe(0);
-});
